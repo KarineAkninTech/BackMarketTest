10a3c5eeeceefa16aaafc4fa7df497e031a3028a
Karine Aknin <karine.aknin@dataforall.fr>
2019-12-27 15:27:40 +0100

    add wget download csv file to ingress folder

--- scripts/generate_datalake.py
+++ scripts/generate_datalake.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import wget
 
 # list of the datalake folder paths to create
 paths = ["./datalake", "./datalake/archive", "./datalake/ingress", "./datalake/raw", "./datalake/raw/copyRawFiles", "./datalake/raw/valid", "./datalake/raw/invalid"]
@@ -20,3 +21,13 @@ try:
 except Exception as error:
     print("ERROR : Unable to create datalake folder architecture: {}".format(str(error)))
     exit(1)
+
+# download product_catalog.csv file to ingress folder
+try:
+    url = "https://backmarket-data-jobs.s3-eu-west-1.amazonaws.com/data/product_catalog.csv"
+    path = os.path.abspath("datalake/ingress/product_catalog.csv")
+    wget.download(url, path)
+    print("\nINFO : Successfully download product_catalog.csv to {}".format(path))
+except Exception as error:
+    print("ERROR: Unable to upload csv file on ingress folder: {}".format(str(error)))
+    exit(-1)
