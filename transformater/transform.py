0d195b57fd3f4f596c7acd879b82a371c013f961
Karine Aknin <karine.aknin@dataforall.fr>
2019-12-27 15:33:24 +0100

    add logger and log file generation

--- transformater/transform.py
+++ transformater/transform.py
@@ -1,2 +1,13 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
+logger = logging.getLogger('pyspark')
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_handler = RotatingFileHandler("transform.log", mode="a", maxBytes=1000000000, backupCount=0, encoding="utf-8")
+file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
 if __name__ == '__main__':
     print("Here you go!")
